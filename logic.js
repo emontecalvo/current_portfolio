349667af9e809fb10b407cae270610ce29b50cac
emontecalvo <emilymontecalvo@gmail.com>
2017-01-03 18:48:18 -0800

    improving ui

--- logic.js
+++ logic.js
@@ -1,17 +1,13 @@
 $( document ).ready(function() {
 	$('.home').on('click', function() {
-		$("html, body").animate({ scrollTop: $('#home_section').offset().top }, 3000);
+		$("html, body").animate({ scrollTop: $('#home_section').offset().top }, 2000);
 	})
 
 	$('.about').on('click', function() {
-		$("html, body").animate({ scrollTop: $('#about_section').offset().top }, 3000);
-	})
-
-	$('.contact').on('click', function() {
-		$("html, body").animate({ scrollTop: $('#contact_section').offset().top }, 3000);
+		$("html, body").animate({ scrollTop: $('#about_section').offset().top }, 2000);
 	})
 
 	$('.projects').on('click', function() {
-		$("html, body").animate({ scrollTop: $('#project_section').offset().top }, 3000);
+		$("html, body").animate({ scrollTop: $('#project_section').offset().top }, 2000);
 	})
 });
